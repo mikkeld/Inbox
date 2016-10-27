5ea1e4cc6832c4bc785ff684c8ec13d6063ff018
Mikkel Dengsoe <mikkelden@gmail.com>
2016-10-27 10:25:00 +0100

    Inbox component initialized

--- inbox/src/app/app.module.ts
+++ inbox/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InboxComponent } from './inbox/inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,

25fd896ae0986da2e14e8aa587d51bc4c10ec514
Mikkel Dengsoe <mikkelden@gmail.com>
2016-10-27 10:24:11 +0100

    Email class complete

--- inbox/src/app/email.ts
+++ inbox/src/app/email.ts
@@ -1,3 +1,15 @@
+import { Person } from './person.ts'
+
 export class Email {
-  constructor()
+  constructor(receiver: Person, sender: Person, date: Date, content: string, title: string) {
+    this.receiver = receiver;
+    this.sender = sender;
+    this.date = date;
+    this.content = content;
+    this.title = title;
+  }
+
+  timeSinceReceived(): DateTimeFormat {
+    return Date.now() - this.date;
+  }
 }
--- inbox/src/app/person.ts
+++ inbox/src/app/person.ts
@@ -1,2 +1,13 @@
 export class Person {
+  constructor(authId: int, firstName: string, lastName: string, profilePicturePath: string, happiness: string) {
+    this.authId=  authId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profilePicturePath = profilePicturePath;
+    this.happiness = happiness;
+  }
+
+  fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }

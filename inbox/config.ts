32b2772ef11e45fc60d728b60578a74ab0c8dd86
Mikkel Dengsoe <mikkelden@gmail.com>
2016-10-27 16:20:31 +0100

    Firebase up

--- inbox/config.ts
+++ inbox/config.ts
@@ -1,1 +1,6 @@
-
+export class Config {
+  apiKey: "AIzaSyBFl2Lre7t7u-8cwtYUu0k3gjrD0iQ3cWs";
+  authDomain: "friendlychat-61656.firebaseapp.com";
+  databaseURL: "https://friendlychat-61656.firebaseio.com";
+  storageBucket: "friendlychat-61656.appspot.com";
+}
--- inbox/src/app/app.module.ts
+++ inbox/src/app/app.module.ts
@@ -7,7 +7,23 @@ import { AppComponent } from './app.component';
 import { InboxComponent } from './inbox/inbox.component';
 
 import { AppRoutingModule }     from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+// import { Config } from '../../config';
 import { ComposeComponent } from './compose/compose.component';
+
+// export const firebaseConfig = {
+//   apiKey: config.apiKey,
+//   authDomain: config.authDomain,
+//   databaseURL: config.databaseURL,
+//   storageBucket: config.storageBucket
+// };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFl2Lre7t7u-8cwtYUu0k3gjrD0iQ3cWs",
+  authDomain: "friendlychat-61656.firebaseapp.com",
+  databaseURL: "https://friendlychat-61656.firebaseio.com",
+  storageBucket: "friendlychat-61656.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -19,7 +35,8 @@ import { ComposeComponent } from './compose/compose.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]

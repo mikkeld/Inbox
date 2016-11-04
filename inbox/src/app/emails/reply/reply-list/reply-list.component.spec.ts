e340d542e52f0216e5f38b2aae7a8977f2ab7e16
Mikkel Dengsoe <mikkelden@gmail.com>
2016-11-04 12:42:02 +0000

    Reply list initiated

---
+++ inbox/src/app/emails/reply/reply-list/reply-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ReplyListComponent } from './reply-list.component';
+
+describe('Component: ReplyList', () => {
+  it('should create an instance', () => {
+    let component = new ReplyListComponent();
+    expect(component).toBeTruthy();
+  });
+});

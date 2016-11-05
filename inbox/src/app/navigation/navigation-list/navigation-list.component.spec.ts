bd8939321cd518f06e7698da61a9971bad85d691
Mikkel Dengsoe <mikkelden@gmail.com>
2016-11-05 16:00:04 +0000

    Initializing navigation list

---
+++ inbox/src/app/navigation/navigation-list/navigation-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NavigationListComponent } from './navigation-list.component';
+
+describe('Component: NavigationList', () => {
+  it('should create an instance', () => {
+    let component = new NavigationListComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ inbox/src/app/navigation/navigation-list/navigation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-list',
+  templateUrl: './navigation-list.component.html',
+  styleUrls: ['./navigation-list.component.css']
+})
+export class NavigationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

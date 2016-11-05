acb5c961a6ebe832166202c8de563c5f92c17591
Mikkel Dengsoe <mikkelden@gmail.com>
2016-11-05 15:52:49 +0000

    User of login component initialized

---
+++ inbox/src/app/navigation/user-or-login/user-or-login.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { UserOrLoginComponent } from './user-or-login.component';
+
+describe('Component: UserOrLogin', () => {
+  it('should create an instance', () => {
+    let component = new UserOrLoginComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ inbox/src/app/navigation/user-or-login/user-or-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-or-login',
+  templateUrl: './user-or-login.component.html',
+  styleUrls: ['./user-or-login.component.css']
+})
+export class UserOrLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

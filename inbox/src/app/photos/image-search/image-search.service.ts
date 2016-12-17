86cdab9aa33633f928034b71a28f25a515dfe223
Mikkel Dengsoe <mikkelden@gmail.com>
2016-12-17 12:16:25 +0000

    Photo search working

--- inbox/src/app/photos/image-search/image-search.service.ts
+++ inbox/src/app/photos/image-search/image-search.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {AngularFire} from "angularfire2";
 import {Observable} from "rxjs";
 import {IPhoto} from "../shared/photo";
+import {AuthService} from "../../auth/auth.service";
 
 @Injectable()
 export class ImageSearchService {
 
   private tags: Observable<string[]>;
+  private photoPath: string;
 
-  constructor(private af: AngularFire) {
+  constructor(private af: AngularFire, private authService: AuthService) {
+    this.photoPath = `photos/${this.authService.id}`;
     this.tags = this.uniqueCategories();
   }
 
@@ -20,7 +23,7 @@ export class ImageSearchService {
   }
 
   private uniqueCategories(): Observable<any[]> {
-    return this.af.database.list('photos/Imf4nFal01MofFYqOe9I8LcfhX22')
+    return this.af.database.list(this.photoPath)
       .map(photos => photos.map(photo => photo.tags)
       .reduce((allTags, current) => {
         return allTags.concat(
